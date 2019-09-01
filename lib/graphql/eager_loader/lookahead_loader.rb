01fd45e5a926804da6452b8d02de41b3bbc54857
Viktor Fonic <viktor.fonic@gmail.com>
2019-09-01 15:06:40 +0200

    Update documentation; yield scoped model and associations in given block

--- lib/graphql/eager_loader/lookahead_loader.rb
+++ lib/graphql/eager_loader/lookahead_loader.rb
@@ -2,11 +2,17 @@
 
 module Graphql::EagerLoader
   class LookaheadLoader
-    # call this method with
+    # Call this method like this:
     # Graphql::EagerLoader::LookaheadLoader.call(Project, lookahead)
+    #
+    # Here's a full example with 'graphql' field:
+    #
+    # field :projects, Types::ProjectType, null: true, extras: [:lookahead]
+    # def projects(lookahead:)
+    #   Graphql::EagerLoader::LookaheadLoader.call(Project, lookahead).all # or: .where(published: true)
+    # end
     def self.call(model_class, lookahead, &block)
       associations = self.associations(model_class, lookahead)
-
       eager_load_associations(model_class, associations, &block)
     end
 
@@ -19,26 +25,34 @@ module Graphql::EagerLoader
     end
 
     def self.selections(lookahead)
-      if lookahead.selections.map(&:name) == %i[edges nodes]
-        lookahead.selections.second.selections
+      selection_names = lookahead.selections.map(&:name)
+      if selection_names == %i[edges nodes] || selection_names == [:nodes]
+        lookahead.selections.find { |lookahead| lookahead.name == :nodes }.selections
       else
         lookahead.selections
       end
     end
 
+    # This method allows you to add additional functionality while eager loading app's models.
+    # For example you can do stuff like (notice the location of `.all` below):
+    #
+    # field :projects, Types::ProjectType, null: true, extras: [:lookahead]
+    # def projects(lookahead:)
+    #   Graphql::EagerLoader::LookaheadLoader.call(Project, lookahead) do |eager_loaded_model, associations|
+    #     associations.values.each do |association|
+    #       if association.instance_of?(ActiveRecord::Reflection::HasManyReflection)
+    #         eager_loaded_model = eager_loaded_model.merge(association.klass.where(published: true))
+    #       end
+    #     end
+    #     eager_loaded_model
+    #   end.all
+    # end
     def self.eager_load_associations(model_class, associations, &block)
       return model_class if associations.blank?
 
       eager_loaded_model = model_class.eager_load(*associations.keys)
 
-      yield if block?
-
-      associations.values.each do |association|
-        if association.instance_of?(ActiveRecord::Reflection::HasManyReflection)
-          # TODO: scope based on actual model policy
-          eager_loaded_model = eager_loaded_model.merge(EventPolicy::Scope.new(nil, Event).resolve)
-        end
-      end
+      eager_loaded_model = yield eager_loaded_model, associations if block_given?
 
       eager_loaded_model
     end
